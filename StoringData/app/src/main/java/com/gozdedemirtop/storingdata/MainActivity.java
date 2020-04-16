c1a6420819a242255c7a4a7cdcc02eca2ee3da94
Gozde <gozdedemirtop@gmail.com>
2020-04-16 20:38:59 +0300

    Alert Dialog

--- StoringData/app/src/main/java/com/gozdedemirtop/storingdata/MainActivity.java
+++ StoringData/app/src/main/java/com/gozdedemirtop/storingdata/MainActivity.java
@@ -1,13 +1,16 @@
 package com.gozdedemirtop.storingdata;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,20 +40,58 @@ public class MainActivity extends AppCompatActivity {
 
     public void save(View view){
 
-        if(!editText.getText().toString().matches("")){
-            int userAge = Integer.parseInt(editText.getText().toString());
-            textView.setText("Your Age: " + userAge);
+        AlertDialog.Builder alertSave = new AlertDialog.Builder(this);
 
-            sharedPreferences.edit().putInt("storeAge",userAge).apply();
-        }
+        alertSave.setTitle("Save");
+        alertSave.setMessage("Are you sure ?");
+
+        alertSave.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(!editText.getText().toString().matches("")){
+                    int userAge = Integer.parseInt(editText.getText().toString());
+                    textView.setText("Your Age: " + userAge);
+
+                    sharedPreferences.edit().putInt("storeAge",userAge).apply();
+                }
+            }
+        });
+
+        alertSave.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Not Saved", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alertSave.show();
     }
 
     public void delete(View view){
-        int deleteData = sharedPreferences.getInt("storeAge",0);
 
-        if (deleteData!=0){
-            sharedPreferences.edit().remove("storeAge").apply();
-            textView.setText("Your Age: ");
-        }
+        AlertDialog.Builder alertDelete = new AlertDialog.Builder(this);
+
+        alertDelete.setTitle("Delete");
+        alertDelete.setMessage("Are you sure ?");
+
+        alertDelete.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int deleteData = sharedPreferences.getInt("storeAge",0);
+
+                if (deleteData!=0){
+                    sharedPreferences.edit().remove("storeAge").apply();
+                    textView.setText("Your Age: ");
+                }
+            }
+        });
+
+        alertDelete.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Not Delete", Toast.LENGTH_LONG).show();
+            }
+        });
+        alertDelete.show();
     }
 }
